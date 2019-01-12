5ef3d9f7d0e4cbe7586259f480bd31c262e70222
JarretYingling <44506154+JarretYingling@users.noreply.github.com>
2019-01-11 22:56:41 -0500

    debugged time calcs and formatting

--- js/script.js
+++ js/script.js
@@ -15,21 +15,28 @@ $(document).ready(function () {
 
     var db = firebase.database();
 
+    let timeFormat = "ddd, MMM DD @ hh:mm A";
+    let width = $(window).width();
+    switch (width < 768) {
+        case true:
+            timeFormat = "ddd hh:mma";
+    }
+    
     db.ref().on("child_added", function (snapshot) {
-        console.log(snapshot);
         console.log(snapshot.val());
+        let nextTrain = snapshot.val().first;
+        console.log(nextTrain);
+        console.log(snapshot.val().freq);
+        while (moment(nextTrain).isBefore(moment().unix())) {
+            nextTrain = moment.unix(nextTrain).add(snapshot.val().freq, "minutes").unix();
+        }
         $("#table").append(`
         <tr>
             <td>${snapshot.val().name}</td>
             <td>${snapshot.val().dest}</td>
             <td>${snapshot.val().freq}</td>
-            <td>${snapshot.val().first}</td>
-            <td>${snapshot.val().first}</td>
-            <!--
-            <td>{moment.unix(snapshot.val().next).format("HH:mm")}</td>
-            <td>{moment.unix(snapshot.val().next, "minutes")}</td>
-            <td>{snapshot.val().next * moment.unix(snapshot.val().start, "months")}</td>
-            -->
+            <td>${moment.unix(nextTrain).format(timeFormat)}</td>
+            <td>${moment.unix(nextTrain).fromNow()}</td>
         </tr>
         `);
     });
@@ -42,7 +49,7 @@ $(document).ready(function () {
             name: $("#name").val(),
             dest: $("#dest").val(),
             freq: $("#freq").val(),
-            first: moment($("#first").val(), "HH:mm").format("X")
+            first: moment($("#first").val(), "HH:mm").unix()
         });
 
         $("#name").val("");
@@ -51,7 +58,122 @@ $(document).ready(function () {
         $("#first").val("");
     });
 
-    console.log(moment().format("MM/DD/YYYY hh:mm:ss"));
+    $("#restore").on("click", function (event) {
+        console.log(event);
+        event.preventDefault();
+        $("#table").empty();
+        initializeTrains();
+    });
 
+    function initializeTrains() {
+        db.ref().set("");
+
+        console.log(moment().unix());
+
+        db.ref().push({
+            name: "Trenton Express",
+            dest: "Trenton",
+            freq: 25,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 0,
+                'minute': 15,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+
+        db.ref().push({
+            name: "Oregon Trail",
+            dest: "Salem, Oregon",
+            freq: 3600,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 1,
+                'minute': 39,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+
+        db.ref().push({
+            name: "Midnight Carriage",
+            dest: "Philadelphia",
+            freq: 15,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 0,
+                'minute': 5,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+
+        db.ref().push({
+            name: "Sing Sing Caravan",
+            dest: "Atlanta",
+            freq: 45,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 0,
+                'minute': 38,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+
+        db.ref().push({
+            name: "Boston Bus",
+            dest: "Boston",
+            freq: 65,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 0,
+                'minute': 20,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+
+        db.ref().push({
+            name: "California Caravan",
+            dest: "San Francisco",
+            freq: 6000,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 1,
+                'minute': 25,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+
+        db.ref().push({
+            name: "Analben's Train",
+            dest: "Florida",
+            freq: 25,
+            first: moment().set({
+                'year': 2019,
+                'month': 0,
+                'date': 11,
+                'hour': 0,
+                'minute': 08,
+                'second': 0,
+                'millisecond': 0
+            }).unix() //.format("X")
+        });
+    }
 
 });
